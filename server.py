a829ed5dc1eee34f35811246237d1d55429e1c10
Manaswi Saha <manaswi@cs.washington.edu>
2017-12-07 11:54:28 -0800

    Added ability to add more metadata fields displayed on the UI

--- server.py
+++ server.py
@@ -9,20 +9,26 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
-@app.route("/<name>", methods=['GET', 'POST'])
-def server(name='user1'):
-
-	errors = []
+@app.route("/<username>", methods=['GET', 'POST'])
+def server(username='user1'):
 
 	# Default filename
 	filename = 'test1.jpeg'
 	filepath = url_for('static', filename='images/' + filename)
 
+	content = {
+		"username": username,
+		"errors": [],
+		"filename": filename,
+		"filepath": filepath
+	}
+	errors = []
+	
 	if request.method == "POST":
 		try:
 
-			current_image = filename = request.form['current_image']
-			filepath = url_for('static', filename='images/' + filename)
+			current_image = content['filename'] = request.form['current_image']
+			content['filepath'] = url_for('static', filename='images/' + content['filename'])
 			if ('question1' in request.form and 
 				'question2' in request.form and 
 				'question3' in request.form):
@@ -43,36 +49,31 @@ def server(name='user1'):
 
 				if (current_img_idx + 1) >= n_files:
 					print ("No files left")
-					return render_template('thankyou.html', name=name)
+					return render_template('thankyou.html', username=username)
 				else:
 					next_image_idx = current_img_idx + 2
 					print("Next image index: ", next_image_idx)
-					filename = 'test' + str(next_image_idx) + '.jpeg'
-					filepath = url_for('static', filename='images/' + filename)
+					content['filename'] = 'test' + str(next_image_idx) + '.jpeg'
+					content['filepath'] = url_for('static', filename='images/' + content['filename'])
 
 
 				print("Question1: " + question1)
 				print("Question2: " + question2)
 				print("Question3: " + question3)
 				print("Current: " + current_image)
-				print("Next: " + filename)
+				print("Next: " + content['filename'])
 			
 			else:
-				errors.append("Some or all fields are empty!")
+				content['errors'].append("Some or all fields are empty!")
 
 
 		except Exception as ex:
-			errors.append(ex.message)
+			content['errors'].append(ex.message)
 			print(traceback.format_exc())
 	
 	# Get list of images from the folder and the associated metadata from the database
 	
-	if len(errors) > 0:
-		print("Errors: ", len(errors))
-		return render_template('tool.html', name=name, filename=filename, filepath=filepath,
-		 errors=errors)
-	else:
-		return render_template('tool.html', name=name, filename=filename, filepath=filepath)
+	return render_template('tool.html', **content)
 
 if __name__ == '__main__':
 	app.run()
