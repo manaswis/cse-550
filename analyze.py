a9cf984313509266696fb9f8e844672d7af8ae30
Manaswi Saha <manaswi@cs.washington.edu>
2017-12-10 03:07:10 -0800

    Middle of fixing Math domain error due to log 0

--- analyze.py
+++ analyze.py
@@ -1,10 +1,12 @@
 
 import math
+import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
 matplotlib.style.use('ggplot')
+pd.set_option('display.max_rows', 500)
 
 def plot_results(plot_df, filename):
 	plot_df.index = plot_df.image_id
@@ -15,6 +17,7 @@ def plot_results(plot_df, filename):
 	plt.savefig('analysis/' + filename, format="pdf", bbox_inches='tight', dpi=500)
 	# plt.show()
 
+image_id_list = range(1,11)
 
 # Get input files
 response_df = pd.read_csv('response.csv')
@@ -22,11 +25,13 @@ gt_df = pd.read_csv('ground_truth.csv')
 
 response_df = response_df[response_df['username'] != 'manaswi']
 
-# Format dataframe to have image_ids as rows and each question's answer as column
+gt_df.index = image_id_list
+gt_df = gt_df.drop(['image_id'], axis=1)
 
-image_ids = range(1,11)
+# Format dataframe to have image_id_list as rows and each question's answer as column
+
 rows = []
-for image_id in image_ids:
+for image_id in image_id_list:
 
 	r_df = response_df[response_df.img_id == image_id]
 
@@ -43,18 +48,21 @@ for image_id in image_ids:
 
 		rows.append((image_id, answer, user_confidence, percent_agreement, username))
 			
-analyze_df = pd.DataFrame(rows, columns=['image_id', 'vote', 'confidence', 'per_agreement', 'username'])
+analyze_df = pd.DataFrame(rows, columns=['image_id', 'vote', 'confidence', 'percent_agreement', 'username'])
+
+# To handle 0s in log transformation for SP algo, changing 1.0 agreement to 0.99
+analyze_df.loc[analyze_df['percent_agreement'] == 1.0, 'percent_agreement'] = 0.99
 print analyze_df
 
 """
 Method 1: majority voting
 """
 results = []
-for idx, row in gt_df.iterrows():
+for image_id in image_id_list:
 
-	image_id = row['image_id']
-	correct = row['correct']
-	a_df = analyze_df[analyze_df.img_id == image_id]
+	correct = gt_df.ix[image_id]['correct']
+
+	a_df = analyze_df[analyze_df.image_id == image_id]
 	counts = a_df['vote'].value_counts()
 
 	if 'yes' in counts: 
@@ -67,12 +75,18 @@ for idx, row in gt_df.iterrows():
 	else: 
 		no_count = 0
 
-	results.append((image_id, yes_count,no_count, correct))
+	if yes_count > no_count:
+		selected_answer = 'yes'
+	else:
+		selected_answer = 'no'
 
-mj_results = pd.DataFrame(results, columns=['image_id', 'yes', 'no', 'correct'])
-mj_results.index = mj_results.image_id
+	results.append((image_id, yes_count,no_count, correct, selected_answer))
+
+mj_results = pd.DataFrame(results, columns=['image_id', 'yes', 'no', 'correct','predicted'])
+mj_results.index = image_id_list
 mj_results = mj_results.drop(['image_id'], axis=1)
-print mj_results
+mj_results['predicted'] = np.where(mj_results['yes'] > mj_results['no'], 'yes', 'no')
+print "\n Majority Voting Results\n", mj_results
 
 # Plot results
 # plot_results(mj_results, 'majority_voting.png')
@@ -83,42 +97,92 @@ Relies on Bayesian Truth Serum (BTS) score
 """
 
 n = 11 # number of respondents
-for image_id in image_ids:
-	a_df = analyze_df[analyze_df.img_id == image_id]
+results = []
+for image_id in image_id_list:
+	a_df = analyze_df[analyze_df.image_id == image_id]
 	
-	# Step 1a: Average mean x_k of the votes for each answer choice
+	print "Image: ", image_id
+	# Step 1a: Average mean xk_mean of the votes for each answer choice
 	xk_mean_yes = mj_results.ix[image_id]['yes'] / n
 	xk_mean_no = mj_results.ix[image_id]['no'] / n
 
-	# Step 1b: Geometric mean y_k of the predictions of percent agreement with answer k
+	print "step1a:", xk_mean_yes, xk_mean_no
+
+	# Step 1b: Geometric mean yk_gmean of the predictions of percent agreement with answer k
 	yk_logsum_yes = 0
 	yk_logsum_no = 0
-	for idx, row in analyze_df.iterrows():
+	for idx, row in a_df.iterrows():
 
-		if row['vote'] = 'yes':
+		if row['vote'] == 'yes':
 			yk_logsum_yes += math.log(row['percent_agreement'])
 			yk_logsum_no += math.log(1 - row['percent_agreement'])
 		else:
 			yk_logsum_yes += math.log(1 - row['percent_agreement'])
 			yk_logsum_no += math.log(row['percent_agreement'])
+		print yk_logsum_yes
 
 	yk_gmean_yes = yk_logsum_yes / n
 	yk_gmean_no = yk_logsum_no / n
 
+	print "step1b:", yk_gmean_yes, yk_gmean_no
+
 	# Step 2: BTS score u_r for each respondent r
 	bts_scores = []
-	for idx, row in analyze_df.iterrows():
+	for idx, row in a_df.iterrows():
 
-		if row['vote'] = 'yes':
-			u_r = math.log(xk_mean_yes / yk_gmean_yes) + (xk_mean_yes * math.log(row['percent_agreement'] / xk_mean_yes) + 
-														  xk_mean_no *  math.log((1 - row['percent_agreement']) / xk_mean_no))
+		if row['vote'] == 'yes':
+			if (yk_gmean_yes == 0):
+				first_term = 0
+			else:
+				print yk_gmean_yes
+				first_term = math.log(xk_mean_yes / yk_gmean_yes)
+
+			if xk_mean_yes == 0:
+				 second_term_a = 0
+			else:
+				second_term_a = xk_mean_yes * math.log(row['percent_agreement'] / xk_mean_yes)
+
+			if xk_mean_no == 0:
+				second_term_b = 0
+			else:
+				second_term_b = xk_mean_no *  math.log((1 - row['percent_agreement']) / xk_mean_no)
+			u_r =  first_term + (second_term_a + second_term_b)
+		
 		else:
-			u_r = math.log(xk_mean_no / yk_gmean_no) + (xk_mean_yes * math.log((1 - row['percent_agreement']) / xk_mean_yes) + 
-														  xk_mean_no *  math.log(row['percent_agreement'] / xk_mean_no))
+			if (yk_gmean_no == 0):
+				first_term = 0
+			else:
+				first_term = math.log(xk_mean_no / yk_gmean_no)
 
-		bts_scores.append((row['username'], u_r))
-	bts_r_df = pd.DataFrame(bts_scores, columns=['username', 'bts_score'])
+			if xk_mean_yes == 0:
+				 second_term_a = 0
+			else:
+				second_term_a = xk_mean_yes * math.log((1 - row['percent_agreement']) / xk_mean_yes)
 
+			if xk_mean_no == 0:
+				second_term_b = 0
+			else:
+				second_term_b = xk_mean_no *  math.log(row['percent_agreement'] / xk_mean_no)
+			
+			u_r =  first_term + (second_term_a + second_term_b)
+
+		bts_scores.append((row['username'], u_r, row['vote']))
+	bts_r_df = pd.DataFrame(bts_scores, columns=['username', 'bts_score', 'vote'])
+
+	# Step 3: Average BTS score ur_mean_k of all respondents endoring answer k
+	ur_mean_yes = bts_r_df[bts_r_df.vote == 'yes'].sum()/ (n * xk_mean_yes)
+	ur_mean_no = bts_r_df[bts_r_df.vote == 'no'].sum()/ (n * xk_mean_no)
+
+	# Step 4: Choose the answerk that has the maximum score
+	if ur_mean_yes > ur_mean_no:
+		selected_answer = 'yes'
+	else:
+		selected_answer = 'no'
+
+	results.append((image_id, gt_df.ix[image_id]['correct'], selected_answer))
+
+sp_results = pd.DataFrame(results, columns=['image_id', 'correct', 'predicted'])
+print "\n Surprisingly Popular (SP) Results\n", sp_results
 
 
 
